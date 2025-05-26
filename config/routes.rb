4a613b22f2cc8aebb4593044ffb39af77839d63d
Matthieu Moloko <matthieu.moloko1506@gmail.com>
2025-05-26 17:48:22 +0200

    Refacto totale

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # Routes des devis
+  resources :quotes, only: [:index, :new, :create, :show, :edit, :update]
 
   # Page d'accueil en racine
   root to: "pages#home"
-
-  # Page d'accueil accessible aussi par /home
-  get "/home", to: "users#home"
-
-  # Formulaire d'inscription
-  get "/user/new", to: "users#new"
-  post "/user", to: "users#create"
-
-  # Routes des devis
-  scope :user do
-    resources :quotes, only: [:index, :new, :create, :show, :edit, :update]
-  end
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end

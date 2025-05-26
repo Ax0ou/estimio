b09d936071348dc3abeff6bca2d18da38c88223f
Axel Alvarade <axel.alvarade@hotmail.fr>
2025-05-26 15:22:20 +0200

    Route added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,21 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # Page d'accueil en racine
+  root to: "users#home"
+
+  # Page d'accueil accessible aussi par /home
+  get "/home", to: "users#home"
+
+  # Formulaire d'inscription
+  get "/user/new", to: "users#new"
+  post "/user", to: "users#create"
+
+  # Routes des devis
+  scope :user do
+    resources :quotes, only: [:index, :new, :create, :show, :edit, :update]
+  end
+
   # Defines the root path route ("/")
   # root "posts#index"
 end

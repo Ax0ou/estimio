bd3c47691f02d7b45d36e81ea3e28a0bd2b3cea0
Axel Alvarade <axel.alvarade@hotmail.fr>
2025-05-26 16:30:41 +0200

    Home page go have a look

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def home
+  end
+
+  def new
+    @craftman = Craftman.new
+  end
+
+  def create
+    @craftman = Craftman.new(craftman_params)
+    if @craftman.save
+      redirect_to user_quotes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def craftman_params
+    params.require(:craftman).permit(:first_name, :last_name, :siret, :company_name, :address, :phone_number)
+  end
 end

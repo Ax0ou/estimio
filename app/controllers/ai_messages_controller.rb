d41b794a5b5f286d3cdbe22cc41512e5e111f9d6
PIERRE-BAPTISTE VIOUGEAT <PBVIOUGEATPRO@GMAIL.COM>
2025-05-27 16:14:33 +0200

    fe

---
+++ app/controllers/ai_messages_controller.rb
@@ -0,0 +1,9 @@
+class AiMessagesController < ApplicationController
+  def new
+    
+  end
+
+  def create
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   # Routes des devis
-  resources :quotes, only: [:index, :new, :create, :show, :edit, :update]
+  resources :quotes, only: [:index, :new, :create, :show, :edit, :update] do
+    resources :ai_message, only: [:new, :create]
+  end
 
   # Page d'accueil en racine
   root to: "pages#home"

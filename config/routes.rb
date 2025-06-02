90c54ca13e0b99f77f97dbcac6bfdbcc2683fa9b
PIERRE-BAPTISTE VIOUGEAT <PBVIOUGEATPRO@GMAIL.COM>
2025-06-02 16:57:45 +0200

    DB and routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # Routes des devis
-  resources :quotes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
-    resources :ai_messages, only: [:new, :create]
+  resources :companies, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :clients, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :quotes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+      resources :sections, only: [:new, :create, :edit, :update, :destroy] do
+        resources :ai_messages, only: [:new, :create]
+        resources :line_items, only: [:new, :create, :edit, :update, :destroy]
+      end
+    end
   end
-
-  resources :clients
 
   # Page d'accueil en racine
   root to: "pages#home"
